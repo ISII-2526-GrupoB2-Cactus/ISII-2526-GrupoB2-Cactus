476538c885981c4ed5c50037c05ce1e3806049cd
MARÍA MARTÍNEZ GONZÁLEZ <Maria.Martinez128@alu.uclm.es>
2025-10-06 21:12:10 +0200

    #5 Creacion y edicion de la clase Review

---
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class Review
+    {
+
+
+        public Review()
+        {
+            //Constructor vacio
+        }
+
+        public Review(int customerId, int customerCount, DateTime dateOfReview, int overallRating, bool reviewed, string reviewTitle, ReviewItem reviewItem)
+        {
+            CustomerId = customerId;
+            CustomerCount = customerCount;
+            DateOfReview = dateOfReview;
+            OverallRating = overallRating;
+            Reviewed = reviewed;
+            ReviewTitle = reviewTitle;
+            ReviewItem = reviewItem;
+        }
+
+        public int CustomerId { get; set; }
+
+        [Range(1, 200, ErrorMessage = "Minimum 1, Maximum 200")]
+        public int CustomerCount { get; set; }
+
+        public DateTime DateOfReview { get; set; }
+
+        [Range(1, 200, ErrorMessage = "Minimum 1, Maximum 200")]
+        public int OverallRating { get; set; }
+
+        public bool Reviewed { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name can be neither longer than 50 characters nor shorter than 10.", MinimumLength = 10)]
+        public string ReviewTitle { get; set; }
+
+        public ReviewItem ReviewItem { get; set; } //Atributo para la relacion
+
+    }
+}
