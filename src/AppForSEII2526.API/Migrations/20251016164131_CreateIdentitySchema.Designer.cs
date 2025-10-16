ec1bdd5bb3d7fb8cfb013fe0c3ced1b95e5c6fb2
organ <organ@LAPTOP-RJ7FHV3U>
2025-10-16 18:43:48 +0200

    #37 Se ha editado la migracion

--- src/AppForSEII2526.API/Migrations/20251016164131_CreateIdentitySchema.Designer.cs
+++ src/AppForSEII2526.API/Migrations/20251016164131_CreateIdentitySchema.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AppForSEII2526.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251008154647_CreateIdentitySchema")]
+    [Migration("20251016164131_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
         /// <inheritdoc />
