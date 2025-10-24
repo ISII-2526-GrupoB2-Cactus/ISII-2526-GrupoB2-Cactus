54549763180421f0b3b46d593a46efcf33d38caa
organ <organ@LAPTOP-RJ7FHV3U>
2025-10-24 14:26:26 +0200

    #64 He editado la clase DevicesController y DeviceParaAlquilarDTO. Revision para tutoria

---
+++ src/AppForSEII2526.API/Controllers/DevicesController.cs
@@ -0,0 +1,61 @@
+﻿using AppForSEII2526.API.DTOs.DeviceDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Drawing2D;
+using System.Net;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<DevicesController> _logger;
+
+        public DevicesController(ApplicationDbContext context, ILogger<DevicesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+
+        
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<DeviceParaAlquilarDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetDevicesForReview(string? model, double? priceForRent)
+        {
+            var devices = await _context.Device
+                .Include(d => d.Model)
+                .Where(d =>
+                    ((string.IsNullOrEmpty(model)) || d.Model.Name.Contains(model)) &&
+                    ((priceForRent == null) || d.PriceForRent == priceForRent)
+                )
+                .OrderBy(d => d.PriceForRent)
+                //.ThenBy(d => d.Year)
+                .Select(d => new DeviceParaAlquilarDTO(
+                    d.Id,
+                    d.Name,
+                    d.Model,
+                    d.Brand,
+                    d.Year,
+                    d.Color,
+                    d.PriceForRent
+                ))
+                .ToListAsync();
+
+            return Ok(devices);
+        }
+        
+
+
+      
+
+
+
+
+    }
+}
+
---
+++ src/AppForSEII2526.API/DTOs/DeviceParaAlquilarDTO/DeviceParaAlquilarDTO.cs
@@ -0,0 +1,53 @@
+﻿namespace AppForSEII2526.API.DTOs.DeviceDTO
+{
+    public class DeviceParaAlquilarDTO
+    {
+
+        //CONSTRUCTOR
+        
+
+        public DeviceParaAlquilarDTO(int id, string name, Model model, string brand, int year, string color, double priceForRent)
+        {
+            Id = id;
+            Name = name;
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            PriceForRent = priceForRent;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
+        public string Name { get; set; }
+
+
+        public Model Model { get; }
+
+
+        [Required]
+        [StringLength(50, ErrorMessage = "La marca no puede tener más de 50 caracteres")]
+        public string Brand { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "El color no puede tener más de 50 caracteres")]
+        public string Color { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(0.5, float.MaxValue, ErrorMessage = "El precio minimo es 0.5 ")]
+        [Display(Name = "Precio de alquiler")]
+        [Precision(10, 2)]
+        public double PriceForRent { get; set; }
+
+       
+
+
+
+        }
+}
