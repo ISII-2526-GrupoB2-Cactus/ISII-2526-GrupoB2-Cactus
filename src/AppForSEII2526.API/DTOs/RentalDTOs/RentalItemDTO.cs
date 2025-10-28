1cbf2228f98f98a5e2267eee80569f0848ab0cf0
organ <organ@LAPTOP-RJ7FHV3U>
2025-10-28 17:45:26 +0100

    #109 Se ha editado RentalItemDTO

---
+++ src/AppForSEII2526.API/DTOs/RentalDTOs/RentalItemDTO.cs
@@ -0,0 +1,43 @@
+﻿namespace AppForSEII2526.API.DTOs.DeviceDTO
+{
+    public class DispositivoAlquilerItemDTO
+    {
+        public DispositivoAlquilerItemDTO(int deviceId, string name, string brand, string model, double priceForRent, int quantity = 1)
+        {
+            DeviceId = deviceId;
+            Name = name;
+            Brand = brand;
+            Model = model;
+            PriceForRent = priceForRent;
+            Quantity = quantity;
+        }
+
+        public int DeviceId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public double PriceForRent { get; set; }
+
+        public int Quantity { get; set; } = 1;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DispositivoAlquilerItemDTO dTO &&
+                   DeviceId == dTO.DeviceId &&
+                   Name == dTO.Name &&
+                   Brand == dTO.Brand &&
+                   Model == dTO.Model &&
+                   PriceForRent == dTO.PriceForRent &&
+                   Quantity == dTO.Quantity;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(DeviceId, Name, Brand, Model, PriceForRent, Quantity);
+        }
+    }
+}
