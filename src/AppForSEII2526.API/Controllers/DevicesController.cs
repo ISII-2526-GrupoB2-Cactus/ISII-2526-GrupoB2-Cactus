51be182dc4b7a45ee324743c3f187da314864402
MARÍA MARTÍNEZ GONZÁLEZ <Maria.Martinez128@alu.uclm.es>
2025-10-28 15:50:27 +0100

    #65 Cambio en DevicesControllerMaria

--- src/AppForSEII2526.API/Controllers/DevicesController.cs
+++ src/AppForSEII2526.API/Controllers/DevicesController.cs
@@ -25,26 +25,7 @@ namespace AppForSEII2526.API.Controllers
         }
 
 
-        //Ejemplo de operacion
-        /*
-        [HttpGet]
-        [Route("[action]")]
-        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
-        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
-        {
-            if (op2 == 0)
-            {
-                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
-                return BadRequest("op2 must be different from 0");
-            }
-            decimal result = decimal.Round(op1 / op2, 2);
-            return Ok(result);
-
-
-        }
-        */
-
+        
 
         [HttpGet]
         [Route("[action]")]
