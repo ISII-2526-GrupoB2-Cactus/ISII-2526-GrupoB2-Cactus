5f2df6fa1e6318197af5828c47cdb1f11f8d3163
MARÍA MARTÍNEZ GONZÁLEZ <Maria.Martinez128@alu.uclm.es>
2025-10-28 15:42:08 +0100

    #65 Modificacion DTOs

---
+++ src/AppForSEII2526.API/DTOs/ReviewDTOs/ReviewDetailDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace AppForSEII2526.API.DTOs.ReviewDTOs
+{
+    public class ReviewDetailDTO : ReviewForCreateDTO
+    {
+        public ReviewDetailDTO(int id,
+                          DateTime reviewDate,
+                          string customerUserName,
+                          string customerNameSurname,
+                          IList<ReviewItemDTO> reviewItems)
+       : base(customerUserName,
+              customerNameSurname,
+              reviewDate,
+              reviewItems)
+        {
+            Id = id;
+            ReviewDate = reviewDate;
+        }
+
+        public int Id { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReviewDetailDTO dto &&
+                   base.Equals(obj) &&
+                   Id == dto.Id &&
+                   CompareDate(ReviewDate, dto.ReviewDate);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Id, ReviewDate);
+        }
+    }
+}
