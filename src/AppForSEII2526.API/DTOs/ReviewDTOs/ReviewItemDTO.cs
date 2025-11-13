be93adbfd08927f69b43aeca47b1cf0643824194
MARÍA MARTÍNEZ GONZÁLEZ <Maria.Martinez128@alu.uclm.es>
2025-11-13 12:01:34 +0100

    #118 Modificacion de la clase ReviewDetailDTO

--- src/AppForSEII2526.API/DTOs/ReviewDTOs/ReviewItemDTO.cs
+++ src/AppForSEII2526.API/DTOs/ReviewDTOs/ReviewItemDTO.cs
@@ -1,18 +1,35 @@
-﻿namespace AppForSEII2526.API.DTOs.ReviewDTOs
+﻿using System.ComponentModel.DataAnnotations;
+namespace AppForSEII2526.API.DTOs.ReviewDTOs
 {
     public class ReviewItemDTO
     {
-        public ReviewItemDTO(int deviceId, int rating, string comments = "")
+        //Asi como de cada dispositivo su nombre, modelo, año, puntuación y el comentario
+        public ReviewItemDTO() { }
+        public ReviewItemDTO(int deviceId, int rating, string? comments)
         {
+            //En este constructor estan los datos OBLIGATORIOS
             DeviceId = deviceId;
             Rating = rating;
             Comments = comments;
         }
-        public ReviewItemDTO()
+        public ReviewItemDTO(int deviceId, string name, string model, int year, int rating, string? comments)
         {
+            //En este constructor estan todos los datos QUE SE MUESTRAN DEL DISPOSITIVO
+            DeviceId = deviceId;
+            Name = name;
+            Model = model;
+            Year = year;
+            Rating = rating;
+            Comments = comments;
         }
 
         public int DeviceId { get; set; }
+
+        [StringLength(30, ErrorMessage = "El nombre no puede tener más de 30 caracteres")]
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+        public int Year { get; set; }
 
         [Required]
         [Range(1, 5, ErrorMessage = "La puntuación debe estar entre 1 y 5.")]
@@ -23,15 +40,18 @@
 
         public override bool Equals(object? obj)
         {
-            return obj is ReviewItemDTO dto &&
-                   DeviceId == dto.DeviceId &&
-                   Rating == dto.Rating &&
-                   Comments == dto.Comments;
+            return obj is ReviewItemDTO dTO &&
+                   DeviceId == dTO.DeviceId &&
+                   Name == dTO.Name &&
+                   Model == dTO.Model &&
+                   Year == dTO.Year &&
+                   Rating == dTO.Rating &&
+                   Comments == dTO.Comments;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(DeviceId, Rating, Comments);
+            return HashCode.Combine(DeviceId, Name, Model, Year, Rating, Comments);
         }
     }
 }
